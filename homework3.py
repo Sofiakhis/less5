8c0e1f4429f22d18a022dec131849a59765912cb
Sofiakhis <sofiakhismatova@mail.ru>
2024-05-18 20:39:24 +0300

    Add files via upload

---
+++ homework3.py
@@ -0,0 +1,11 @@
+my_string = input('Введите ваш город: ')
+# print(my_string.upper())
+#print(my_string.lower())
+#print(my_string.replace(' ', ''))
+#print(my_string[0:1])
+#print(my_string[5])
+print(my_string[-1])
+
+
+
+
